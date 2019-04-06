f093b5bda51a23dc7496b62f41b371713f4ee1f2
tnandate <nandate.takuya@moneyforward.co.jp>
2019-04-06 22:20:36 +0900

    in the Editor component pass in an activeId prop to the EventList component

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -8,14 +8,14 @@ class EventList extends React.Component {
   }
 
   renderEvents() {
-    const { events } = this.props;
+    const { activeId, events } = this.props;
     events.sort(
       (a, b) => new Date(b.event_date) - new Date(a.event_date),
     );
 
     return events.map(event => (
       <li key={event.id}>
-        <Link to={`/events/${event.id}`}>
+        <Link to={`/events/${event.id}`} className={activeId === event.id ? 'active' : ''}>
           {event.event_date}
           {' - '}
           {event.event_type}
@@ -35,10 +35,12 @@ class EventList extends React.Component {
 }
 
 EventList.propTypes = {
+  activeId: PropTypes.number,
   events: PropTypes.arrayOf(PropTypes.object),
 };
 
 EventList.defaultProps = {
+  activeId: undefined,
   events: [],
 };
 
