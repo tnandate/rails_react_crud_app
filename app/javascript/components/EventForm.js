0dc43d9796586cec8cfea51b4ba455ef81bb4d8b
tnandate <nandate.takuya@moneyforward.co.jp>
2019-04-13 17:10:09 +0900

    add a Cancel button to the form

--- app/javascript/components/EventForm.js
+++ app/javascript/components/EventForm.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Pikaday from 'pikaday';
 import { formatDate, isEmptyObject, validateEvent } from '../helpers/helpers';
 import 'pikaday/css/pikaday.css';
+import { Link } from 'react-router-dom';
 
 class EventForm extends React.Component {
   constructor(props) {
@@ -85,11 +86,14 @@ class EventForm extends React.Component {
 
   render() {
     const { event } = this.state;
-
+    const cancelURL = event.id? `/events/${event.id}` : '/events';
+    const title = event.id ? `${event.event_date} - ${event.event_type}` : 'New Event'
     return (
       <div>
-        <h2>New Event</h2>
+        <h2>{title}</h2>
+
         {this.renderErrors()}
+
         <form className="eventForm" onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="event_type">
@@ -129,6 +133,7 @@ class EventForm extends React.Component {
           </div>
           <div className="form-actions">
             <button type="submit">Save</button>
+            <Link to={cancelURL}>Cancel</Link>
           </div>
         </form>
       </div>
